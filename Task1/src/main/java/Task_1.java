33cd798a5c94c64cc2e5caa4d359399d70867d04
Eugene <zohan1234567@gmail.com>
2019-08-01 18:45:25 +0300

    second commit Task_1

--- Task1/src/main/java/Task_1.java
+++ Task1/src/main/java/Task_1.java
@@ -1,23 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task_1 {
     public static void main(String[] args) {
-// 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
 
+       // 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a size of array:");
         int n = scanner.nextInt();
         int[] array = new int[n];
         System.out.println("Enter natural number k ");
         int k = scanner.nextInt();
-        for (int i = 0; i < array.length; i++) {
+
+        int sum = 0;
+        for (int i=0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 100);
-            System.out.print("|" + array[i]);
         }
-        for (int value : array) {
-            if (k % value == 0)
-                System.out.print("\n this number is multiple: " + value);
+        System.out.print(Arrays.toString(array)+ "\n");
+
+        for (int i=0; i < array.length; i++) {
+            if (array[i] % k == 0) {
+                sum = sum + array[i];
+            }
         }
+        System.out.print("The sum of elements that are multiples of "+ k + " is " + sum);
     }
 }
 
