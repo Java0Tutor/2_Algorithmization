86215f8bc5f1c591c7abf34ea38d72cb04efb3a0
Eugene <zohan1234567@gmail.com>
2019-08-08 00:09:54 +0300

    forth commit Task_7

---
+++ Task7/src/main/java/Task_7.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_7 {
+    public static void main(String[] args) {
+
+     //7. Даны действительные числа a 1 , a 2 ,  , a n . Найти
+        //max( a 1 + a 2 n , a 2 + a 2 n − 1 ,  , a n + a n + 1 ) .
+        int [] mas;
+        int n, i, j;
+        int max;
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Enter the size of array n: ");
+            n = Integer.parseInt(reader.readLine());
+
+            //инициализация и заполнение массива рандомными значениями от -1000 до 1000
+            mas = new int[2 * n];
+            for (i = 0; i < mas.length; i++) {
+                int sing = Math.random() < 0.5 ? -1 : 1;
+                mas[i] = (int) (Math.random() * 1000) * sing;
+
+            }
+
+            i = 0;
+            j = mas.length - 1;
+            max = Integer.MIN_VALUE;
+            for (; i < j; i++, j--) {
+                max = Integer.max(mas[i] + mas[j], max);
+            }
+
+            System.out.println("\n" + max);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
