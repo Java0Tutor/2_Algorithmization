bc0600d61c2514dcee8087931bb7afaab17d8b61
Eugene <zohan1234567@gmail.com>
2019-08-08 00:15:36 +0300

    fifth commit Task_7

--- Task7/src/main/java/Task_7.java
+++ Task7/src/main/java/Task_7.java
@@ -17,11 +17,11 @@ public class Task_7 {
             n = Integer.parseInt(reader.readLine());
 
             //инициализация и заполнение массива рандомными значениями от -1000 до 1000
-            mas = new int[2 * n];
+            mas = new int[n];
             for (i = 0; i < mas.length; i++) {
                 int sing = Math.random() < 0.5 ? -1 : 1;
                 mas[i] = (int) (Math.random() * 1000) * sing;
-
+                System.out.print(mas[i]+", ");
             }
 
             i = 0;
@@ -31,7 +31,7 @@ public class Task_7 {
                 max = Integer.max(mas[i] + mas[j], max);
             }
 
-            System.out.println("\n" + max);
+            System.out.println("\nmax value is " + max);
         } catch (IOException e) {
             e.printStackTrace();
         }
